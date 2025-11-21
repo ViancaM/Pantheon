32006a307e3ff80d0f452852dbd377f8124a06f2
ViancaM <123019949+ViancaM@users.noreply.github.com>
2025-11-21 14:20:10 -0600

    Removed Django

--- game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('game/', views.game_view, name='game'),
-    path('save-settings/', views.save_settings, name='save_settings'),
-]
